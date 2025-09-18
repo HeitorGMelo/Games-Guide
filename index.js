169fdd96ea5a559396d1156ef5da3b808824f9ea
Heitor Melo <heitormelopro@gmail.com>
2025-09-18 16:28:40 -0300

    Adicionando uma nova seção

--- index.js
+++ index.js
@@ -75,3 +75,96 @@ function login(){
 function cadastro(){
     alert(`🚧Em Construção🚧`)
 }
+
+//Mudando Categorias- NOTICIAS - ESPORTS - PROMOÇÕES
+
+//Noticias
+function Noticias(){
+    //Noticia 01
+    document.getElementsByClassName("filho")[0].style.backgroundImage = "url(src/Witcher4.svg)"
+    document.getElementsByClassName("tituloCard")[0].innerHTML = "The Witcher 4: Nova Demonstração Técnica Impressiona"
+    document.getElementsByClassName("textoCard")[0].innerHTML = "A CD Projekt Red, em colaboração com a Epic Games, apresentou uma nova demonstração técnica de The Witcher 4 durante o evento State of Unreal 2025. A demo, exibida ao vivo em um PlayStation 5 padrão, rodou a 60 quadros por segundo com ray tracing ativado, utilizando o Unreal Engine 5.6. A demonstração apresentou Ciri, a nova protagonista, explorando ambientes detalhados, incluindo uma floresta exuberante e um mercado vibrante chamado Valdrest. A equipe enfatizou o foco na otimização para consoles, visando uma experiência fluida e acessível para os jogadores."
+
+    //Noticia 02
+    document.getElementsByClassName("filho")[1].style.backgroundImage = "url(src/Cyberpunk.svg)"
+    document.getElementsByClassName("tituloCard")[1].innerHTML = "Patch 2.31 de Cyberpunk 2077 || Também em desenvolvimento: Cyberpunk 2"
+    document.getElementsByClassName("textoCard")[1].innerHTML = "Recentemente, a CD Projekt Red lançou o patch 2.31 para Cyberpunk 2077, que traz algumas melhorias bastante aguardadas pela comunidade. || Cyberpunk 2 (codinome “Project Orion”) entrou na fase de pré-produção, com cerca de 96 desenvolvedores envolvidos até abril deste ano"
+
+    //Noticia 03
+    document.getElementsByClassName("filho")[2].style.backgroundImage = "url(src/Playstation.svg)"
+    document.getElementsByClassName("tituloCard")[2].innerHTML = "State of Play"
+    document.getElementsByClassName("textoCard")[2].innerHTML = "A Sony confirmou que o próximo evento State of Play trará novidades sobre títulos exclusivos do PlayStation 5, alimentando rumores sobre novas IPs e possíveis atualizações de franquias de peso como God of War e Spider-Man."
+
+    //Notica 04
+    document.getElementsByClassName("filho")[3].style.backgroundImage = "url(src/gta6.svg)"
+    document.getElementsByClassName("tituloCard")[3].innerHTML = "AAA/AAAA e preços"
+    document.getElementsByClassName("textoCard")[3].innerHTML = "Jogos AAA e AAAA estão cada vez mais caros, com títulos de grande escala podendo ultrapassar os R$ 500 no lançamento, reflexo dos custos de produção recordes e do investimento em experiências de alto nível, gerando debates sobre acessibilidade e sustentabilidade no mercado gamer."
+
+    //Noticia 05
+    document.getElementsByClassName("filho")[4].style.backgroundImage = "url(src/Ubisoft.svg)"
+    document.getElementsByClassName("tituloCard")[4].innerHTML = "Ubisoft: Ações despencam após adiamento de Assassin’s Creed Shadows"
+    document.getElementsByClassName("textoCard")[4].innerHTML = "A Ubisoft anunciou o adiamento de Assassin’s Creed Shadows para 20 de março de 2025, visando aprimorar a qualidade do jogo com base no feedback dos jogadores. O título, ambientado no Japão feudal, já havia sido postergado anteriormente de novembro de 2024 para fevereiro de 2025. Essa decisão gerou uma reação negativa no mercado financeiro, com as ações da empresa caindo quase 20%, atingindo o menor valor em mais de uma década."
+}
+
+
+
+
+
+
+//E-SP0RTS
+function Esports(){ 
+    //Noticia 01
+    document.getElementsByClassName("filho")[0].style.backgroundImage = "url(src/theInternational.svg)"
+    document.getElementsByClassName("tituloCard")[0].innerHTML = "Brasil brilha no The International 2025"
+    document.getElementsByClassName("textoCard")[0].innerHTML = "O Brasil teve uma participação destacada no The International 2025, o maior torneio de Dota 2 do mundo. A equipe HEROIC, com representantes brasileiros, alcançou o top 6 do mundial após vencer a Tundra Esports por 2 a 0. Além disso, a PARIVISION, comandada pelo técnico brasileiro Astini, garantiu o top 3 ao derrotar a Xtreme Gaming por 2 a 0. Juntas, as equipes acumularam mais de R$ 2 milhões em premiações "
+
+    //Noticia 02
+    document.getElementsByClassName("filho")[1].style.backgroundImage = "url(src/CBDE.svg)"
+    document.getElementsByClassName("tituloCard")[1].innerHTML = "CBDEL 2025 movimenta o cenário nacional"
+    document.getElementsByClassName("textoCard")[1].innerHTML = "O Campeonato Brasileiro de E-sports (CBDEL) 2025, organizado pela CBDEL e patrocinado pela B3, ocorreu entre 4 e 13 de março. O torneio reuniu equipes de Mobile Legends: Bang Bang, destacando-se como uma das principais competições nacionais do ano "
+
+    //Noticia 03
+    document.getElementsByClassName("filho")[2].style.backgroundImage = "url(src/LTASUL.svg)"
+    document.getElementsByClassName("tituloCard")[2].innerHTML = "LTA Sul 2025: Novas equipes e desafios"
+    document.getElementsByClassName("textoCard")[2].innerHTML = "O Circuito Desafiante 2025, competição oficial do Tier 2 brasileiro de League of Legends, teve início em março com a participação de 10 equipes, incluindo Corinthians E-Sports, Dopamina E-Sport, Flamengo MDL, KaBuM! IDL, LOS, RATZ, RED Canids Kalunga Academy, Rise Gaming, STELLAE Gaming e Vivo Keyd Stars Academy. A primeira fase foi disputada em formato 'melhor de 3', com todas as equipes se enfrentando uma vez"
+
+    //Notica 04
+    document.getElementsByClassName("filho")[3].style.backgroundImage = "url(src/Riot.svg)"
+    document.getElementsByClassName("tituloCard")[3].innerHTML = "Riot Games Brasil lança hub de competições comunitárias"
+    document.getElementsByClassName("textoCard")[3].innerHTML = "Para fortalecer o cenário grassroots, a Riot Games Brasil firmou parceria com a Monosup para lançar uma plataforma chamada “Competições Organizadas”. Essa iniciativa oferece ferramentas, suporte e estrutura para organizadores de eventos menores de League of Legends, Wild Rift, Valorant e Teamfight Tactics. O objetivo é simplificar a organização, garantir alinhamento com campanhas oficiais da Riot e melhorar a experiência de jogadores e público local."
+
+    //Noticia 05
+    document.getElementsByClassName("filho")[4].style.backgroundImage = "url(src/Liquid.svg)"
+    document.getElementsByClassName("tituloCard")[4].innerHTML = "Team Liquid reforça presença no Brasil"
+    document.getElementsByClassName("textoCard")[4].innerHTML = "A Team Liquid confirmou participação na Brasil Game Show 2025, com estande próprio e diversas atrações, como parte de seu esforço para se aproximar ainda mais da comunidade gamer brasileira. Além disso, o time está investindo em melhorias estruturais, projetos sustentáveis para atletas, inclusão e parcerias de marca, destacando a importância do Brasil em sua estratégia global de crescimento."
+
+
+}
+//PROMOÇÕES
+function Promoções(){
+        //Noticia 01
+    document.getElementsByClassName("filho")[0].style.backgroundImage = "url(src/steam.svg)"
+    document.getElementsByClassName("tituloCard")[0].innerHTML = "Steam Summer Sale 2025"
+    document.getElementsByClassName("textoCard")[0].innerHTML = "A Steam está com sua edição de verão (Summer Sale) rolando, com descontos bem agressivos. Vale a pena ficar de olho: essas promoções da Steam reúnem muitos jogos, desde AAA até indies, e é possível pegar títulos que normalmente você consideraria “caros”."
+
+    //Noticia 02
+    document.getElementsByClassName("filho")[1].style.backgroundImage = "url(src/psstore.svg)"
+    document.getElementsByClassName("tituloCard")[1].innerHTML = "Promoções na PlayStation Store (Brasil)"
+    document.getElementsByClassName("textoCard")[1].innerHTML = "A PS Store costuma fazer várias promoções grandes; aqui vão alguns destaques: CYberpunk 2077, Red Dead Redemption 2, Alan Wake 2, Final Fantasy, Dynasty Warriors entre outros "
+
+    //Noticia 03
+    document.getElementsByClassName("filho")[2].style.backgroundImage = "url(src/Nintendo.svg)"
+    document.getElementsByClassName("tituloCard")[2].innerHTML = "Nintendo Switch — Promoção “Recordar é Viver"
+    document.getElementsByClassName("textoCard")[2].innerHTML = "Para quem tem um Switch, essa é ótima: vários jogos títulos nostálgicos ou clássicos estão com até 80% de desconto como Red Dead Redemption, Hogwarts Legacy, Grand Theft Auto e outros"
+
+    //Notica 04
+    document.getElementsByClassName("filho")[3].style.backgroundImage = "url(src/Epic.svg)"
+    document.getElementsByClassName("tituloCard")[3].innerHTML = "Epic Games Store – Jogos Gratuitos e Descontos de até 95%"
+    document.getElementsByClassName("textoCard")[3].innerHTML = "A Epic Games Store continua sua tradição semanal de oferecer jogos gratuitos. Atualmente, estão disponíveis gratuitamente Monument Valley 2, Ghostrunner 2 e The Battle of Polytopia. Além disso, de 18 a 25 de setembro, serão oferecidos Project Winter e Samorost 2. Aproveite para adicionar esses títulos à sua biblioteca sem custo algum."
+
+    //Noticia 05
+    document.getElementsByClassName("filho")[4].style.backgroundImage = "url(src/Proximos.svg)"
+    document.getElementsByClassName("tituloCard")[4].innerHTML = "Próximas Promoções Importantes"
+    document.getElementsByClassName("textoCard")[4].innerHTML = "Steam: A próxima grande promoção será a Promoção de Outono (29 de setembro a 6 de outubro), seguida pelo Steam Next Fest (13 a 20 de outubro), que oferecerá demos gratuitas de jogos em desenvolvimento. Epic Games Store: A plataforma está programada para oferecer novos jogos gratuitos de 18 a 25 de setembro, incluindo Project Winter e Samorost 2"
+
+}
